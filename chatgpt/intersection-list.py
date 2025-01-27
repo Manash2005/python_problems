5aff60e81d8b7889ba8261b481e9c13bb5b8e357
Manash2005 <swainm099@gmail.com>
2025-01-27 23:20:51 +0530

    solved intersection of lists

---
+++ chatgpt/intersection-list.py
@@ -0,0 +1,17 @@
+'''Problem Statement: Given two lists of integers, write a program to find their intersection. The intersection of two lists is a new list that contains only the elements that are common to both lists, without duplicates.'''
+num_1 = input("Enter numbers separated with spaces:\n")
+num_2 = input("Enter numberss separated with spaces:\n")
+
+lst_1 = list(map(int, num_1.split()))
+lst_2 = list(map(int, num_2.split()))
+
+lst = []
+
+for i in lst_1:
+    for x in lst_2:
+        if x == i:
+            lst.append(x)
+        else:
+            continue
+
+print(lst)
