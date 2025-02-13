c3ee9fa7e4a49c4153259d1adeb377af4256b354
Manash2005 <swainm099@gmail.com>
2025-02-13 11:31:51 +0530

    solved dracula eats problem on code chef

--- codechef/draculaEats.py
+++ codechef/draculaEats.py
@@ -1,4 +1,10 @@
+# cook your dish here
 T = int(input())
 for i in range(T):
     N = int(input()) # days left for halloween
-    print((N/8)+1)
+    weeks = int(N/7)
+    extra = N%7
+    tuesdays = weeks
+    if extra>=2:
+        tuesdays += 1
+    print(tuesdays)
