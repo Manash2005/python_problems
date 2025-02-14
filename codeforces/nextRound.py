11ffaffb567eb3969e89732a1d49c5cdb73be83c
Manash2005 <swainm099@gmail.com>
2025-02-14 23:59:50 +0530

    solved next round problem on code forces

---
+++ codeforces/nextRound.py
@@ -0,0 +1,9 @@
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+threshold = arr[k-1]
+count = 0
+for score in arr:
+    if score >= threshold and score > 0:
+        count += 1
+
+print(count)
