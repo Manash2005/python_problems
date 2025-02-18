e2d56e212fd530de578f9137b8d2318212256464
Manash2005 <swainm099@gmail.com>
2025-02-18 23:27:03 +0530

    solved air conditioner problem in code chef

--- codechef/airConditionerTemprature.py
+++ codechef/airConditionerTemprature.py
@@ -2,3 +2,16 @@ T = int(input())
 for i in range(T):
     A,B,C = map(int,input().split())
     
+T = int(input())
+for i in range(T):
+    A,B,C = map(int,input().split())
+    if A>=C:
+        if B>=A:
+            print("YES")
+        else:
+            print("NO")
+    else:
+        if B>=C:
+            print("YES")
+        else:
+            print("NO")
